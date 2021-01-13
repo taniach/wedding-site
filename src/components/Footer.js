f511f5ada9ccbdf604c8819725d20f6a77fe6007
Tania Chattopadhyay <tchatterjee5@gmail.com>
2021-01-14 01:20:15 +0800

    Add favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,11 @@ function Footer() {
                 <div className="col-sm-12 text-center content">
                     <span className="to-top-wrapper"><a href="#top" className="to-top"><FontAwesomeIcon icon={faAngleUp} /></a></span>
                     <p>Website coded with lots of <FontAwesomeIcon icon={faHeart} className="heartbeat" /> by Tania</p>
-                    <small className="text-muted"><a href="#code">View on GitHub</a></small>
+                    <small className="text-muted">
+                        <a href="https://github.com/taniach/wedding-site" target="_blank" rel="noreferrer">
+                            View on GitHub
+                        </a>
+                    </small>
                 </div>
             </Row>
         </Container>
