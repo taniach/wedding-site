3aa506f090721dde9dc3e8f96a4724a8bccd5c64
Tania Chattopadhyay <tchatterjee5@gmail.com>
2021-01-14 13:25:55 +0800

    Remove brackets in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             <Row>
                 <div className="col-sm-12 text-center content">
                     <span className="to-top-wrapper"><a href="#top" className="to-top"><FontAwesomeIcon icon={faAngleUp} /></a></span>
-                    <p>Website (coded) with lots of <FontAwesomeIcon icon={faHeart} className="heartbeat" /> by Tania</p>
+                    <p>Website coded with lots of <FontAwesomeIcon icon={faHeart} className="heartbeat" /> by Tania</p>
                     <small className="text-muted">
                         <a href="https://github.com/taniach/wedding-site" target="_blank" rel="noreferrer">
                             View on GitHub
