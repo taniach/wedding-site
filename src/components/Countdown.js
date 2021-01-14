d8d1490568fd3468cb88f06ec5d6e55916258021
Tania Chattopadhyay <tchatterjee5@gmail.com>
2021-01-14 12:35:25 +0800

    Add else condition for time difference

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -11,6 +11,13 @@ function calculateTimeLeft() {
       minutes: Math.floor((difference / 1000 / 60) % 60),
       seconds: Math.floor((difference / 1000) % 60)
     };
+  } else {
+    timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
   }
 
   return timeLeft;
